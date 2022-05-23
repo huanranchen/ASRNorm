96002c953b21914d976332d9733044ef0dcabc34
Andrew Chen <1062446749@qq.com>
2022-05-23 23:24:48 +0800

    Add files via upload

--- ASRNorm.py
+++ ASRNorm.py
@@ -19,8 +19,8 @@ class ASRNorm(nn.Module):
         self.rescale_mean_decoder = nn.Linear(dim // 16, dim)
         self.rescale_var_decoder = nn.Linear(dim // 16, dim)
 
-        self.lambda_1 = nn.Parameter(torch.zeros(dim) - 2)
-        self.lambda_2 = nn.Parameter(torch.zeros(dim) - 2)
+        self.lambda_1 = nn.Parameter(torch.zeros(dim))
+        self.lambda_2 = nn.Parameter(torch.zeros(dim))
 
         self.bias_1 = nn.Parameter(torch.zeros(dim))
         self.bias_2 = nn.Parameter(torch.ones(dim))
@@ -35,20 +35,18 @@ class ASRNorm(nn.Module):
         lambda_2 = torch.sigmoid(self.lambda_2)
         N, C, H, D = x.shape
         x = x.permute(0, 2, 3, 1).reshape(-1, C)
-        mean = x.mean(0)
-        var = x.std(0)
-        standard_encoded = F.relu(self.standard_encoder(x))
-        asr_mean = self.standard_mean_decoder(standard_encoded)
-        asr_var = F.relu(self.standard_var_decoder(standard_encoded))
-        mean = lambda_1 * asr_mean + (1 - lambda_1) * mean
-        var = lambda_2 * asr_var + (1 - lambda_2) * var
+        real_mean = x.mean(0)
+        real_var = x.std(0)
+        asr_mean = self.standard_mean_decoder(F.relu(self.standard_encoder(real_mean.view(1, -1)))).squeeze()
+        asr_var = F.relu(self.standard_var_decoder(F.relu(self.standard_encoder(real_var.view(1, -1))))).squeeze()
+        mean = lambda_1 * asr_mean + (1 - lambda_1) * real_mean
+        var = lambda_2 * asr_var + (1 - lambda_2) * real_var
 
         x = (x - mean) / (var + self.eps)
 
-        rescale_encoded = F.relu(self.rescale_encoder(x))
-        asr_mean = torch.tanh(self.rescale_mean_decoder(rescale_encoded)) + self.bias_1
-        asr_var = torch.sigmoid(self.rescale_var_decoder(rescale_encoded)) + self.bias_2
-        # print(asr_mean, asr_var)
+        asr_mean = torch.tanh(self.rescale_mean_decoder(F.relu(self.rescale_encoder(real_mean.view(1, -1))))).squeeze() + self.bias_1
+        asr_var = torch.sigmoid(
+            self.rescale_var_decoder(F.relu(self.rescale_encoder(real_var.view(1, -1))))).squeeze() + self.bias_2
         x = x * asr_var + asr_mean
         x = x.reshape(N, H, D, C)
         x = x.permute(0, 3, 1, 2)
