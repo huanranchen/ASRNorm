48ba200370cf1739c464fb8e27953f17867a7feb
Andrew Chen <1062446749@qq.com>
2022-11-24 18:45:32 +0800

    chr: augment policy modified

--- data/cifar.py
+++ data/cifar.py
@@ -184,8 +184,10 @@ def get_CIFAR100_train(batch_size=256,
         ])
     else:
         transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            # transforms.ColorJitter(),
             transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
-            # transforms.RandomRotation(5),
+            transforms.RandomRotation(5),
             transforms.ToTensor(),
             transforms.Normalize([0.5071, 0.4867, 0.4408], [0.2675, 0.2565, 0.2761]),
         ])
